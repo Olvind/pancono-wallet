fb887d8893be8744bccc5d2428316aa99cd031f2
Olvind <73144382+Olvind@users.noreply.github.com>
2025-09-03 08:04:57 +0530

    Create app.js

---
+++ static/app.js
@@ -0,0 +1,11 @@
+document.getElementById("create").onclick = () => {
+  alert("Wallet Created ✅");
+};
+
+document.getElementById("import").onclick = () => {
+  alert("Wallet Imported 🔑");
+};
+
+document.getElementById("balance").onclick = () => {
+  alert("Balance: 0.00 PAN 🪙");
+};
